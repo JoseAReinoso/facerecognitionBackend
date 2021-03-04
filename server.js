8fbc38f3099f98a6fc583f59a99367ff08fcf955
Reinoso <jreinoso@amazon.com>
2021-03-04 06:30:04 -0500

    adding new changes made

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
-bodyParser =  require('body-parser')
+const bodyParser =  require('body-parser')
+const cors = require('cors')
+
 
 
 const app = express()
 
 
 app.use(bodyParser.json())
+app.use(cors())
 
 const database = {
     users: [
@@ -35,7 +38,7 @@ app.get('/',(req,res)=> {
 
 app.post('/signin', (req, res)=> {
       if (req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-          res.json('sucess')
+          res.json('Success')
       }else{
           res.status(400).json('error loggin in')
       }
@@ -84,6 +87,6 @@ app.put("/image",(req, res)=> {
 
 })
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('app is running on port' )
 })
